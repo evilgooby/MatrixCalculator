5d7ca797b3202ba5320b14c486998e15a3979225
evilgooby <alxah@yandex.ru>
2024-04-09 22:33:51 +0300

    Добавил функцию нахождения обратно и транспонирования. Сделал проект многофайловым

---
+++ MatrixCalculator/Header.cpp
@@ -0,0 +1,289 @@
+#include"Header.h"
+
+MatrixCalculator::MatrixCalculator(int const SIZE)
+{
+	this->SIZE = SIZE;
+	matrix1 = new double* [SIZE];
+	for (int i = 0; i < SIZE; i++)
+		matrix1[i] = new double[SIZE];
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		for (int j = 0; j < SIZE; j++)
+		{
+			matrix1[i][j] = 0;
+		}
+	}
+}
+
+MatrixCalculator::~MatrixCalculator()
+{
+	for (int i = 0; i < SIZE; i++)
+		delete[] matrix1[i];
+	delete[] matrix1;
+}
+
+void MatrixCalculator::AddMatrix()
+{
+	cout << "Заполним матрицу числами" << endl;
+	for (int i = 0; i < SIZE; i++)
+		for (int j = 0; j < SIZE; j++)
+			cin >> matrix1[i][j];
+}
+
+const void MatrixCalculator::checkMatrix()
+{
+	cout << endl;
+	for (int i = 0; i < SIZE; i++)
+	{
+		for (int j = 0; j < SIZE; j++)
+			cout << matrix1[i][j] << " ";
+		cout << endl;
+	}
+	cout << endl;
+}
+
+const void MatrixCalculator::checkMatrix(double** tempMatrix)
+{
+	cout << endl;
+	for (int i = 0; i < SIZE; i++)
+	{
+		for (int j = 0; j < SIZE; j++)
+			cout << tempMatrix[i][j] << "\t";
+		cout << endl;
+	}
+	cout << endl;
+}
+
+void MatrixCalculator::determinant()
+{
+	cout << findDet(matrix1, SIZE) << endl;
+}
+
+void MatrixCalculator::clearMemory(double** a, int n)
+{
+	for (int i = 0; i < n; i++) {
+		delete[] a[i];
+	}
+	delete[] a;
+}
+
+double** MatrixCalculator::copyMatrix(double** tempMatrix)
+{
+	for (int i = 0; i < SIZE; i++)
+		for (int j = 0; j < SIZE; j++)
+			matrix1[i][j] = tempMatrix[i][j];
+	return matrix1;
+}
+
+double** MatrixCalculator::GetMatr(double** mas, int rows, int cols, int row, int col)
+{
+	int di, dj;
+	double** p = new double* [rows];
+	di = 0;
+	for (int i = 0; i < rows - 1; i++) {
+		if (i == row)
+			di = 1;
+		dj = 0;
+		p[i] = new double[cols];
+		for (int j = 0; j < cols - 1; j++) {
+			if (j == col)
+				dj = 1;
+			p[i][j] = mas[i + di][j + dj];
+		}
+	}
+	return p;
+}
+
+int MatrixCalculator::findDet(double** mas, int n)
+{
+	if (n < 0)
+	{
+		cout << "Определитель вычислить невозможно!";
+		return 0;
+	}
+	else if (n == 1)
+		return mas[0][0];
+	else if (n == 2)
+		return mas[0][0] * mas[1][1] - mas[0][1] * mas[1][0];
+	else
+	{
+		int d = 0;
+		for (int k = 0; k < n; k++)
+		{
+			double** m = new double* [n - 1];
+			for (int i = 0; i < n - 1; i++)
+			{
+				m[i] = new double[n - 1];
+			}
+			for (int i = 1; i < n; i++)
+			{
+				int t = 0;
+				for (int j = 0; j < n; j++)
+				{
+					if (j == k)
+						continue;
+					m[i - 1][t] = mas[i][j];
+					t++;
+				}
+			}
+			d += pow(-1, k + 2) * mas[0][k] * findDet(m, n - 1);
+			clearMemory(m, n - 1);
+		}
+		return d;
+	}
+}
+
+double** MatrixCalculator::Mreverse(double** mas, int m)
+{
+	double** tempMatrix = new double* [m];
+
+	double det = findDet(mas, m);
+	for (int i = 0; i < m; i++)
+	{
+		tempMatrix[i] = new double[m];
+		for (int j = 0; j < m; j++)
+		{
+			tempMatrix[i][j] = findDet(GetMatr(mas, m, m, i, j), m - 1);
+			if ((i + j) % 2 == 1)
+				tempMatrix[i][j] = -tempMatrix[i][j];
+			tempMatrix[i][j] = tempMatrix[i][j] / det;
+		}
+	}
+	return transpone(tempMatrix, m);
+}
+
+void MatrixCalculator::obratnaya()
+{
+	Mreverse(matrix1, SIZE);
+}
+
+double** MatrixCalculator::transpone(double** mas, int m)
+{
+	double** tempMatrix = new double* [m];
+
+	for (int i = 0; i < m; i++)
+	{
+		tempMatrix[i] = new double[m];
+		for (int j = 0; j < m; j++)
+		{
+			tempMatrix[i][j] = mas[j][i];
+		}
+	}
+	copyMatrix(tempMatrix);
+	clearMemory(tempMatrix, m);
+	return matrix1;
+}
+
+double** MatrixCalculator::transpone()
+{
+	double** tempMatrix;
+	tempMatrix = new double* [SIZE];
+	for (int i = 0; i < SIZE; i++)
+	{
+		tempMatrix[i] = new double[SIZE];
+		for (int j = 0; j < SIZE; j++)
+			tempMatrix[i][j] = matrix1[j][i];
+	}
+	copyMatrix(tempMatrix);
+	clearMemory(tempMatrix, SIZE);
+	return matrix1;
+}
+
+void MatrixCalculator::raiseToAPower(int power)
+{
+	double** tempMatrix;
+
+	tempMatrix = new double* [SIZE];
+	for (int i = 0; i < SIZE; i++)
+		tempMatrix[i] = new double[SIZE];
+	for (int i = 0; i < SIZE; i++)
+		for (int j = 0; j < SIZE; j++)
+			tempMatrix[i][j] = 0;
+
+	for (int i = 0; i < SIZE; i++)
+		for (int k = 0; k < SIZE; k++)
+			for (int j = 0; j < SIZE; j++)
+				tempMatrix[i][k] += matrix1[i][j] * matrix1[j][k];
+	copyMatrix(tempMatrix);
+	clearMemory(tempMatrix, SIZE);
+}
+
+void MatrixCalculator::Multiply(int num)
+{
+	for (int i = 0; i < SIZE; i++)
+		for (int j = 0; j < SIZE; j++)
+			matrix1[i][j] *= num;
+}
+
+MatrixCalculator MatrixCalculator::operator*=(const MatrixCalculator& matrix)
+{
+	double** tempMatrix;
+
+	tempMatrix = new double* [SIZE];
+	for (int i = 0; i < SIZE; i++)
+		tempMatrix[i] = new double[SIZE];
+	for (int i = 0; i < SIZE; i++)
+		for (int j = 0; j < SIZE; j++)
+			tempMatrix[i][j] = 0;
+
+	for (int i = 0; i < SIZE; i++)
+		for (int k = 0; k < SIZE; k++)
+			for (int j = 0; j < SIZE; j++)
+				tempMatrix[i][k] += matrix1[i][j] * matrix.matrix1[j][k];
+	
+	copyMatrix(tempMatrix);
+	clearMemory(tempMatrix, SIZE);
+	return *this;
+}
+
+MatrixCalculator MatrixCalculator::operator+=(const MatrixCalculator& matrix)
+{
+	double** tempMatrix;
+
+	tempMatrix = new double* [SIZE];
+	for (int i = 0; i < SIZE; i++)
+		tempMatrix[i] = new double[SIZE];
+	for (int i = 0; i < SIZE; i++)
+		for (int j = 0; j < SIZE; j++)
+			tempMatrix[i][j] = 0;
+
+	for (int i = 0; i < SIZE; i++)
+		for (int k = 0; k < SIZE; k++)
+			for (int j = 0; j < SIZE; j++)
+				tempMatrix[i][k] += matrix1[i][j] + matrix.matrix1[j][k];
+
+	copyMatrix(tempMatrix);
+	clearMemory(tempMatrix, SIZE);
+	return *this;
+}
+
+
+//MatrixCalculator MatrixCalculator::operator*(const MatrixCalculator& matrix)
+//{
+//	double** tempMatrix;
+//
+//	tempMatrix = new double* [SIZE];
+//	for (int i = 0; i < SIZE; i++)
+//		tempMatrix[i] = new double[SIZE];
+//	for (int i = 0; i < SIZE; i++)
+//		for (int j = 0; j < SIZE; j++)
+//			tempMatrix[i][j] = 0;
+//
+//	for (int i = 0; i < SIZE; i++)
+//		for (int k = 0; k < SIZE; k++)
+//			for (int j = 0; j < SIZE; j++)
+//				tempMatrix[i][k] += matrix1[i][j] * matrix.matrix1[j][k];
+//
+//	copyMatrix(tempMatrix);
+//	clearMemory(tempMatrix, SIZE);
+//	return *this;
+//}
+//
+//MatrixCalculator MatrixCalculator::operator=(const MatrixCalculator& matrix)
+//{
+//	for (int i = 0; i < SIZE; i++)
+//		for (int j = 0; j < SIZE; j++)
+//			matrix1[i][j] = matrix.matrix1[i][j];
+//	return *this;
+//}
--- MatrixCalculator/Source.cpp
+++ MatrixCalculator/Source.cpp
@@ -1,127 +1,17 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<cmath>
-using namespace std;
-
-class MatrixCalculator
-{
-public:
-	MatrixCalculator(int const SIZE);
-	MatrixCalculator(int const SIZE, int const SIZE2);
-	~MatrixCalculator();
-
-	void AddMatrix()
-	{
-		cout << "Заполним матрицу числами" << endl;
-		for (int i = 0; i < SIZE; i++)
-			for (int j = 0; j < SIZE; j++)
-				cin >> matrix1[i][j];
-
-	}
-	const void checkMatrix()
-	{
-		for (int i = 0; i < SIZE; i++)
-		{
-			for (int j = 0; j < SIZE; j++)
-				cout << matrix1[i][j] << " ";
-			cout << endl;
-		}
-		cout << endl;
-	}
-
-	void clearMemory(int** a, int n) { //Функция освобождения памяти, выделенной под двумерный динамический массив
-		for (int i = 0; i < n; i++) {
-			delete[] a[i];
-		}
-		delete[] a;
-	}
-
-	void determinant()
-	{
-		cout << findDet(matrix1, SIZE) << endl;
-	}
-
-private: int findDet(int** a, int n) { //Рекурсивная функция вычисления определителя матрицы
-	if (n == 1)
-		return a[0][0];
-	else if (n == 2)
-		return a[0][0] * a[1][1] - a[0][1] * a[1][0];
-	else {
-		int d = 0;
-		for (int k = 0; k < n; k++) {
-			int** m = new int* [n - 1];
-			for (int i = 0; i < n - 1; i++) {
-				m[i] = new int[n - 1];
-			}
-			for (int i = 1; i < n; i++) {
-				int t = 0;
-				for (int j = 0; j < n; j++) {
-					if (j == k)
-						continue;
-					m[i - 1][t] = a[i][j];
-					t++;
-				}
-			}
-			d += pow(-1, k + 2) * a[0][k] * findDet(m, n - 1);
-			clearMemory(m, n - 1); //Освобождаем память, выделенную под алгебраическое дополнение
-		}
-		return d; //Возвращаем определитель матрицы
-	}
-}
-public:
-
-	void raiseToAPower(int power)
-	{
-		int** tempMatrix;
-		tempMatrix = new int* [SIZE];	// создаем временную матрицу и выделяем под неё память
-		for (int i = 0; i < SIZE; i++)
-			tempMatrix[i] = new int[SIZE];
-
-		for (int i = 0; i < SIZE; i++)
-			for (int j = 0; j < SIZE; j++)
-				tempMatrix[i][j] = 0;
-
-		for (int i = 0; i < SIZE; i++)
-			for (int k = 0; k < SIZE; k++)
-				for (int j = 0; j < SIZE; j++)
-					tempMatrix[i][k] += matrix1[i][j] * matrix1[j][k];
-
-		for (int i = 0; i < SIZE; i++)
-			for (int j = 0; j < SIZE; j++)
-				matrix1[i][j] = tempMatrix[i][j];
-
-		clearMemory(tempMatrix, SIZE);
-	}
-
-	void Multiply(int num)
-	{
-		for (int i = 0; i < SIZE; i++)
-		{
-			for (int j = 0; j < SIZE; j++)
-			{
-				matrix1[i][j] *= num;
-			}
-		}
-	}
-
-
-private:
-
-	int** matrix1;
-	int SIZE;
-};
-
-
+#include"Header.h"
 
 int main()
 {
+	int SIZE;
+	cout << "Введите размер матрицы" << endl; cin >> SIZE;
+
 	setlocale(LC_ALL, "rus");
-	MatrixCalculator m(3);
+	MatrixCalculator m(SIZE);
+	MatrixCalculator m1(SIZE);
+	MatrixCalculator m3(SIZE);
 	m.AddMatrix();
-
-	m.determinant();
-
+	m1.AddMatrix();
+	
 	m.checkMatrix();
 
 	return 0;
@@ -129,18 +19,3 @@ int main()
 
 
 
-MatrixCalculator::MatrixCalculator(int const SIZE)
-{
-	this->SIZE = SIZE;
-	matrix1 = new int* [SIZE];
-	for (int i = 0; i < SIZE; i++)
-		matrix1[i] = new int[SIZE];
-}
-
-MatrixCalculator::~MatrixCalculator()
-{	
-	for (int i = 0; i < SIZE; i++)
-		delete[] matrix1[i];
-	delete[] matrix1;
-
-}
